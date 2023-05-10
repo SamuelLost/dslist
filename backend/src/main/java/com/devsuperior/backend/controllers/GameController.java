26c66b980dabe5e4bdd5ce86adb10eb8c4c7f284
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2023-05-09 23:04:27 -0300

    DTO, repository, controller e service feitos

--- backend/src/main/java/com/devsuperior/backend/controllers/GameController.java
+++ backend/src/main/java/com/devsuperior/backend/controllers/GameController.java
@@ -1,10 +1,24 @@
 package com.devsuperior.backend.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.backend.dto.GameMinDTO;
+import com.devsuperior.backend.services.GameService;
+
 @RestController
-@RequestMapping(value = "/games")
+@RequestMapping(value = "/api/games")
 public class GameController {
     
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        return gameService.findAll();
+    }
 }
--- backend/src/main/java/com/devsuperior/backend/services/GameService.java
+++ backend/src/main/java/com/devsuperior/backend/services/GameService.java
@@ -1,5 +1,23 @@
 package com.devsuperior.backend.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.backend.dto.GameMinDTO;
+import com.devsuperior.backend.entities.Game;
+import com.devsuperior.backend.repositories.GameRepository;
+
+@Service
 public class GameService {
-    
+
+	@Autowired
+	private GameRepository gameRepository;
+	
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(GameMinDTO::new).toList();
+        
+	}
 }
