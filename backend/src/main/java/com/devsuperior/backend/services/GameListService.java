fb5601bb300bdf994175d2dae374339b82b283a6
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2023-05-13 22:20:12 -0300

    Movendo os jogos de posição

--- backend/src/main/java/com/devsuperior/backend/services/GameListService.java
+++ backend/src/main/java/com/devsuperior/backend/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.backend.dto.GameListDTO;
 import com.devsuperior.backend.entities.GameList;
+import com.devsuperior.backend.projections.GameMinProjection;
 import com.devsuperior.backend.repositories.GameListRepository;
+import com.devsuperior.backend.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -16,10 +18,28 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
-	@Transactional(readOnly = true)
+	@Autowired
+	private GameRepository	gameRepository;
+
+	@Transactional(readOnly = true) // readOnly = true garante que não haverá lock no banco
 	public List<GameListDTO> findAll() {
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(GameListDTO::new).toList();
+	}
+
+	@Transactional
+	public void move(Long listId, int sourceIndex, int targetIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(targetIndex, obj);
+		
+		int min = (sourceIndex < targetIndex) ? sourceIndex : targetIndex;
+		int max = (sourceIndex > targetIndex) ? sourceIndex : targetIndex;
+
+		for(int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+
 	}
 
 
